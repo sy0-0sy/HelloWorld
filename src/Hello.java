5e807e8f357ec053fd78bdf27a45e3fc3e81efc1
shenyang <1224647477@qq.com>
2021-04-29 20:16:55 +0800

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,25 @@
+public class Hello implements Runnable{
+    private volatile boolean flag=true;
+    @Override
+    public void run() {
+        int i=0;
+        while(flag){
+            System.out.println("run ...thread"+(++i));
+        }
+    }
+    public void stop(){
+        this.flag=false;
+    }
+
+    public static void main(String[] args) {
+        Hello hello=new Hello();
+        new Thread(hello).start();
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("主线程:  "+i);
+            if(i==900){
+                hello.stop();
+                System.out.println("------子进程停----------");
+            }
+        }
+    }
+}
